caec94867f950680c293bd981f1ad9ac7109cb90
Jeff Pitcher <pit19004@byui.edu>
2022-05-17 17:49:18 -0600

    halfway through

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,26 @@
 //import math;
 fun main(args: Array<String>) {
-    println("What is the price of a child's meal? ");
-    var kidsMealPrice = readln();
-    println(kidsMealPrice);
+    println("What is the price of a child's meal?\n(Please include value with a decimal including the cents): ");
+    val kidsMealPrice = readln();
+    println("The price for a kids meal is $ ${kidsMealPrice}\n");
 //adult_meal = float(input("What is the price of an adult's meal? "))
-//
-//actual_children = int(input("How many children are there? "))
-//
+    println("What is the price of an adult's meal?\n(Please include value with a decimal including the cents): ");
+    val adultMealPrice = readln();
+    println("The price for an adult meal is $ ${adultMealPrice}\n");
+//actual_children = int(input("How many children are there? ")
+    println("How many children are there?\n(Please put in a number without decimals" )
+    val numOfChildren = readln();
+    println("The number of children in your party is: ${numOfChildren}\n");
 //actual_adults = int(input("How many adults are there? "))
-//
+    println("How many adults are there?\n(Please put in a number without decimals");
+    val numOfAdults = readln();
+    println("The number of adults in your party is: ${numOfChildren}\n");
 //tax_initial= float(input("What is the sales tax rate in whole percent? "))
-//
+    println("What is the sales tax rate in whole percent\n(Please include the value with a decimal to the nearest hundredth");
+    val taxInitial = readln();
+    print("The tax applied here is: ${taxInitial}");
 //print(f"Subtotal: ${((child_meal*actual_children)+(adult_meal*actual_adults))}")
-//
+
 //print(f"Sales Tax: ${((((child_meal*actual_children)+(adult_meal*actual_adults))*tax_initial)/100)}")
 //
 //print(f"Total:${(((child_meal*actual_children)+(adult_meal*actual_adults))+(((((child_meal*actual_children)+(adult_meal*actual_adults))*tax_initial)/100)))}")
