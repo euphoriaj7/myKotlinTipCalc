4ad545a495074550e56ee37e68f9ec021ce7bba2
Jeff Pitcher <pit19004@byui.edu>
2022-05-21 17:29:39 -0600

    final push (if it works)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
 import java.math.RoundingMode
+import java.security.KeyStore.TrustedCertificateEntry
 import java.text.DecimalFormat
 
-fun main(args: Array<String>) {
+fun <boolean> main(args: Array<String>) {
     println("What is the price of a child's meal?\n(Please include value with a decimal including the cents): ");
     val kidsMealPrice = readln();
     println("The price for a kids meal is $ ${kidsMealPrice}\n");
@@ -10,7 +11,7 @@ fun main(args: Array<String>) {
     val adultMealPrice = readln();
     println("The price for an adult meal is $ ${adultMealPrice}\n");
 //actual_children = int(input("How many children are there? ")
-    println("How many children are there?\n(Please put in a number without decimals" )
+    println("How many children are there?\n(Please put in a number without decimals")
     val numOfChildren = readln();
     println("The number of children in your party is: ${numOfChildren}\n");
 //actual_adults = int(input("How many adults are there? "))
@@ -23,21 +24,24 @@ fun main(args: Array<String>) {
     println("The tax applied here is: ${taxInitial}%")
     println("-----")
 //print(f"Subtotal: ${((child_meal*actual_children)+(adult_meal*actual_adults))}")
-    val Subtotal = (kidsMealPrice.toDouble()*numOfChildren.toDouble() + (adultMealPrice.toDouble()*numOfAdults.toDouble()))
+    val Subtotal =
+        (kidsMealPrice.toDouble() * numOfChildren.toDouble() + (adultMealPrice.toDouble() * numOfAdults.toDouble()))
     val df = DecimalFormat("#.##")
     df.roundingMode = RoundingMode.DOWN
     val subTotalTaxRoundoff = df.format(Subtotal.toDouble())
     println("Subtotal: $${subTotalTaxRoundoff}")
     println("-----")
 //print(f"Sales Tax: ${((((child_meal*actual_children)+(adult_meal*actual_adults))*tax_initial)/100)}")
-    val subTotalTax = (((kidsMealPrice.toDouble()*numOfChildren.toDouble()) + (adultMealPrice.toDouble()*numOfAdults.toDouble())) * taxInitial.toDouble())/100.0
+    val subTotalTax =
+        (((kidsMealPrice.toDouble() * numOfChildren.toDouble()) + (adultMealPrice.toDouble() * numOfAdults.toDouble())) * taxInitial.toDouble()) / 100.0
     val df2 = DecimalFormat("#.##")
     df.roundingMode = RoundingMode.DOWN
     val subTotalRoundoff = df2.format(subTotalTax.toDouble())
     println("Your tax total is: $${subTotalRoundoff}")
     println("-----")
 //print(f"Total:${(((child_meal*actual_children)+(adult_meal*actual_adults))+(((((child_meal*actual_children)+(adult_meal*actual_adults))*tax_initial)/100)))}")
-    val total = ((kidsMealPrice.toDouble()*numOfChildren.toDouble())+(adultMealPrice.toDouble()*numOfAdults.toDouble()))+((((kidsMealPrice.toDouble()*numOfChildren.toDouble())+(adultMealPrice.toDouble()*numOfAdults.toDouble()))*taxInitial.toDouble())/100.0)
+    val total =
+        ((kidsMealPrice.toDouble() * numOfChildren.toDouble()) + (adultMealPrice.toDouble() * numOfAdults.toDouble())) + ((((kidsMealPrice.toDouble() * numOfChildren.toDouble()) + (adultMealPrice.toDouble() * numOfAdults.toDouble())) * taxInitial.toDouble()) / 100.0)
     val df3 = DecimalFormat("#.##")
     df.roundingMode = RoundingMode.DOWN
     val TotalRoundoff = df3.format(total.toDouble())
@@ -50,7 +54,7 @@ fun main(args: Array<String>) {
     val tipRoundoff = df5.format(tip.toDouble())
     println("Your tip amount is: $$tipRoundoff")
 //print(f"Your new total is ${(tip+(((child_meal*actual_children)+(adult_meal*actual_adults))+(((((child_meal*actual_children)+(adult_meal*actual_adults))*tax_initial)/100))))}
-    val totalWithTip = (tip.toDouble()+total)
+    val totalWithTip = (tip.toDouble() + total)
 //    val df7 = DecimalFormat("#.##")
 //    df.roundingMode = RoundingMode.DOWN
 //    val totalWithTipRoundoff = df7.format(totalWithTip.toDouble())
@@ -59,18 +63,21 @@ fun main(args: Array<String>) {
 //change=((float(input("What is the payment amount in dollars ? ")))-(tip+(((child_meal*actual_children)+(adult_meal*actual_adults))+(((((child_meal*actual_children)+(adult_meal*actual_adults))*tax_initial)/100)))))
     println("what is the payment amount in dollars?\n Please enter an amount that will equal or cover the cost of the bill")
     val payment = readln()
-    val change = ((payment.toDouble()) - (tip.toDouble()+total))
+    val change = ((payment.toDouble()) - (tip.toDouble() + total))
     val df6 = DecimalFormat("#.##")
     df.roundingMode = RoundingMode.DOWN
     val changeRoundoff = df6.format(change.toDouble())
 //print(f"Change: ${change}")
-    while (change >= 0){
-        val df4 = DecimalFormat("#.##")
-        df.roundingMode = RoundingMode.DOWN
-        val TotalRoundoff = df4.format(change.toDouble())
-        print("Your change back is: $${changeRoundoff}\nThanks for coming in")
-        break
-    } //else {
-//
-//    }
-}
+    var customerPaid: Boolean = true
+    while (customerPaid) {
+        if (change >= 0) {
+            val df4 = DecimalFormat("#.##")
+            df.roundingMode = RoundingMode.DOWN
+            val TotalRoundoff = df4.format(change.toDouble())
+            print("Your change back is: $${changeRoundoff}\nThanks for coming in")
+            break
+        } else {
+            print("Please put in an amount that will cover the full cost")
+        }
+    }}
+
